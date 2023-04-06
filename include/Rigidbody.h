57d29a1059b0eb73c47b1f884112d9cf08af9278
Emil Lundström <89786676+Emil1891@users.noreply.github.com>
2023-04-06 18:21:45 +0200

    Comment

--- include/Rigidbody.h
+++ include/Rigidbody.h
@@ -5,7 +5,7 @@
 #include "Collider.h"
 #include <unordered_set>
 
-// OBS DENNA KLASS HAR FORTFARANDE EN DEL BUGGAR. FRÄMST GÄLLANDE ELASTICITET OCH NÄR DEN ANVÄNDS TSM MED PIXELKOLLISIONER.
+// this class works well with the made game but is not ready for general purpose use
 
 namespace engine {
     class Rigidbody : public SpriteComponent
@@ -15,24 +15,24 @@ namespace engine {
 
         void update(std::vector<GameObject*> objs) override; 
 
-        /// <summary> Om gravitationen önskas vara större eller mindre. </summary>
-        /// <param name="mult"> multiplicerar med standardvärdet. 0 för att inte ha nån gravitation. </param>
+        /// <summary> Om gravitationen Ã¶nskas vara stÃ¶rre eller mindre. </summary>
+        /// <param name="mult"> multiplicerar med standardvÃ¤rdet. 0 fÃ¶r att inte ha nÃ¥n gravitation. </param>
         void setGravityMultiplier(const float mult) { gravityMultiplier = mult; }
         float getGravityMultiplier() const { return gravityMultiplier; }
 
-        /// <summary> Sätter hur mycket som figuren ska "studsa" vid kollision. </summary>
-        /// <param name="mult"> faktorn som multipliceras in vid kollision. Lägre = mindre "studs". 0 för att inte ha nån "studs". </param>
+        /// <summary> SÃ¤tter hur mycket som figuren ska "studsa" vid kollision. </summary>
+        /// <param name="mult"> faktorn som multipliceras in vid kollision. LÃ¤gre = mindre "studs". 0 fÃ¶r att inte ha nÃ¥n "studs". </param>
         void setElasticityMultiplier(const float mult) { elasticityMultiplier = mult; }
         float getElasticityMultiplier() const { return elasticityMultiplier; }
 
-        /// <summary> Lägger på en kraft på figuren. OBS att funktionen ser till att det rör sig lika fort oavsett FPS m.h.a Session::deltaTime. </summary>
+        /// <summary> LÃ¤gger pÃ¥ en kraft pÃ¥ figuren. OBS att funktionen ser till att det rÃ¶r sig lika fort oavsett FPS m.h.a Session::deltaTime. </summary>
         /// <param name="vec"> kraften som ska appliceras i form av en Vector(x, y). </param>
         void addForce(const Vector force) { velocity += force; }
         void addForceX(const float x) { velocity.x += x; }
         void addForceY(const float y) { velocity.y += y; }
 
-        /// <summary> Sätter hastigheten som figuren ska röra sig. OBS att funktionen lägger på Session::deltaTime så det rör sig lika oavsett FPS. </summary>
-        /// <param name="vel"> hastigheten i form av en vektor. (finns överlagrad med 2 floats) </param>
+        /// <summary> SÃ¤tter hastigheten som figuren ska rÃ¶ra sig. OBS att funktionen lÃ¤gger pÃ¥ Session::deltaTime sÃ¥ det rÃ¶r sig lika oavsett FPS. </summary>
+        /// <param name="vel"> hastigheten i form av en vektor. (finns Ã¶verlagrad med 2 floats) </param>
         void setVelocity(const Vector vel) { velocity = vel; }
         void setVelocity(const float velX, const float velY) { setVelocity(Vector(velX, velY)); }
         void setVelocityY(const float y) { velocity.y = y; }
@@ -42,10 +42,10 @@ namespace engine {
 
         bool isGrounded() const; 
 
-        // Om den är trigger händer inget vid kollision 
+        // Om den Ã¤r trigger hÃ¤nder inget vid kollision 
         void setIsTrigger(const bool toggle) { isTrigger = toggle; }
 
-        // Om figuren ska sakta ned i x-led när den rör sig, aktivt som standard
+        // Om figuren ska sakta ned i x-led nÃ¤r den rÃ¶r sig, aktivt som standard
         void enableFriction(const bool toggle) { frictionIsActive = toggle; }
         bool hasFriction() const { return frictionIsActive; }
 
@@ -54,17 +54,17 @@ namespace engine {
             NONE = -1, TOP, RIGHT, BOTTOM, LEFT
         };
 
-        bool isTrigger = false; // Om den är trigger ska inget hända vid kollision 
+        bool isTrigger = false; // Om den Ã¤r trigger ska inget hÃ¤nda vid kollision 
         bool frictionIsActive = true;
 
         float gravityMultiplier = 1.0f; 
         Vector velocity = Vector(0, 0); 
         float elasticityMultiplier = 0.5f; 
 
-        void updateGravity(); // Öker farten nedåt i y-axeln (eller saktar ned en rörelse uppåt) om figuren inte står på nåt eller gravitationen är avstängd
-        void decreaseVelocity(); // Sänker hastigheten i x-led (friktion)
+        void updateGravity(); // Ã–ker farten nedÃ¥t i y-axeln (eller saktar ned en rÃ¶relse uppÃ¥t) om figuren inte stÃ¥r pÃ¥ nÃ¥t eller gravitationen Ã¤r avstÃ¤ngd
+        void decreaseVelocity(); // SÃ¤nker hastigheten i x-led (friktion)
 
-        void collided(GameObject* other); // Funktionen som kallas från Collider vid kollision och sedan utför aktionen som behövs, t.ex vänder velY vid kollision med marken
+        void collided(GameObject* other); // Funktionen som kallas frÃ¥n Collider vid kollision och sedan utfÃ¶r aktionen som behÃ¶vs, t.ex vÃ¤nder velY vid kollision med marken
         CollisionSide getCollisionSide(const GameObject* other) const;
         void move(std::vector<GameObject*>& objs);
         void checkIfMoveIsPossible(Vector& newPos, Vector& oldPos, Collider* coll);
